28cce0e5089edfd5a6b186de6b16739f42ec23c7
sonikbro <toliamel@gmail.com>
2019-10-11 13:40:23 +0300

    -f -g

--- summit/custom-trends.js
+++ summit/custom-trends.js
@@ -1,26 +1,43 @@
-setTimeout(function() {
+setTimeout(function () {
 	$('#tildacopy').css("display", "none");
 }, 100);
 if ($(window).width() > 768) {
-	$('.speaker--card').hover(function() {
+	$('.speaker--card').hover(function () {
 		$('.speaker--card').not(this).toggleClass('opacity');
 	});
 };
 if ($(window).width() < 768) {
-	$('.speaker--card').click(function() {
+	$('.speaker--card').click(function () {
 		$(this).toggleClass('hover-card');
 		$('.speaker--card').not(this).removeClass('hover-card').toggleClass('opacity');
 	});
 };
-$('a.box--btn').on('click', function(e) {
+$('a.box--btn').on('click', function (e) {
 	$('html,body').stop().animate({
-		scrollTop: $('#form').offset().top + 50
+		scrollTop: $('#form').offset().top + -100
 	}, 1000);
 	e.preventDefault();
 });
-$('#call-link').on('click', function(e) {
+$('#call-link').on('click', function (e) {
 	$('html,body').stop().animate({
-		scrollTop: $('#form').offset().top + 10
+		scrollTop: $('#form').offset().top + -100
 	}, 1000);
+	e.preventDefault();
+});
+
+$('.box1').on('click', function(e) {
+	$('#tabs1').click();
+	e.preventDefault();
+});
+$('.box2').on('click', function(e) {
+	$('#tabs2').click();
+	e.preventDefault();
+});
+$('.box3').on('click', function(e) {
+	$('#tabs3').click();
+	e.preventDefault();
+});
+$('.box4').on('click', function(e) {
+	$('#tabs4').click();
 	e.preventDefault();
 });
