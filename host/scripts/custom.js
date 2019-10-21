4484c81c193b076fbf7ac4305a7d0efc716cb90c
sonikbro <toliamel@gmail.com>
2019-10-21 11:09:20 +0300

    big update summit, add files, -g -f

--- host/scripts/custom.js
+++ host/scripts/custom.js
@@ -1,4 +1,4 @@
-$(document).ready(function() {
+$(document).ready(function () {
 	setBackground()
 });
 $(window).resize(setBackground)
@@ -10,7 +10,7 @@ function setBackground() {
 		$('.hero').css('background-image', 'url(' + $('.hero').data('img-lg') + ')')
 	}
 }
-$(".accorden-item__title").on("click", function() {
+$(".accorden-item__title").on("click", function () {
 	$(".accorden-item__title").removeClass("is-open"),
 		$(this).addClass("is-open");
 	var t = $(this).next();
@@ -30,7 +30,7 @@ function o(t, e) {
 			height: i
 		}, e)
 }
-$('.feed-button').on('click', function(e) {
+$('.feed-button').on('click', function (e) {
 	if (!$(this).prev('div').hasClass('feed-max')) {
 		$('.feed--text').removeClass('feed-max');
 		$(this).prev('div').toggleClass('feed-max');
@@ -43,24 +43,36 @@ $('.feed-button').on('click', function(e) {
 });
 $('#allrecords').after('<div id="copydel"></div>');
 $(".program__item:first").addClass('program-item-block-max');
-setTimeout(function() {
+setTimeout(function () {
 	$('#tildacopy').css("display", "none");
 }, 100);
-$(".program__item").click(function() {
+$(".program__item").click(function () {
 	$(this).toggleClass('program-item-block-max');
 	$('.program__item').not(this).removeClass('program-item-block-max');
 });
-$("#packageModal").click(function() {
+$("#packageModal").click(function () {
 	$(".modal--itemsmob").addClass('active');
 	$(".modal--itemsmob-bg").addClass('on');
 });
-$("#packageModalClose").click(function() {
+$("#packageModalClose").click(function () {
 	$(".modal--itemsmob").removeClass('active');
 	$(".modal--itemsmob-bg").removeClass('on');
 });
-$('a.box--btn').on('click', function(e) {
+$('a.box--btn').on('click', function (e) {
 	$('html,body').stop().animate({
 		scrollTop: $('#form').offset().top + 260
 	}, 1000);
 	e.preventDefault();
 });
+
+if ($(window).width() > 768) {
+	$('.speaker--card').hover(function () {
+		$('.speaker--card').not(this).toggleClass('opacity');
+	});
+};
+if ($(window).width() < 768) {
+	$('.speaker--card').click(function () {
+		$(this).toggleClass('hover-card');
+		$('.speaker--card').not(this).removeClass('hover-card').toggleClass('opacity');
+	});
+};
