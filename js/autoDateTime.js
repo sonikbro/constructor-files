34d0dac21ba3f68afd28b527cb21ff28aab485bd
Anatoliy Melnyk <toliamel@gmail.com>
2020-05-01 14:38:48 +0300

    feat auto setup global date and time

---
+++ js/autoDateTime.js
@@ -0,0 +1,17 @@
+var table = [ 
+    { key: '612', date: '6 мая', time: '12:00'},
+    { key: '619', date: '6 мая', time: '19:00'},
+    { key: '712', date: '7 мая', time: '12:00'},
+    { key: '719', date: '7 мая', time: '19:00'},
+    { key: '812', date: '8 мая', time: '12:00'},
+    { key: '819', date: '8 мая', time: '19:00'},
+    { key: '912', date: '9 мая', time: '12:00'},
+    { key: '919', date: '9 мая', time: '19:00'},
+ ];
+
+var dateTime = new Date(),
+    currentTime = +(dateTime.getDate() + '' + dateTime.getHours());
+
+const found = table.find(el => el.key > currentTime);
+$tableDate = $('.cDate').text(found.date);
+$tableTime = $('.cTime').text(found.time);
